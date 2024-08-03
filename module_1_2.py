e0284eedba8551a6096a7e775503c4826ba7852a
KapitanRoss <kapital.jazz@yandex.ru>
2024-08-03 20:31:39 +0300

    Update module_1_2.py

--- module_1_2.py
+++ module_1_2.py
@@ -2,4 +2,4 @@ Home_work = 12
 Number_of_hours = 1.5
 Course = 'Python'
 Time1 = Number_of_hours/Home_work
-print("Курс:",Course,',' "всего задач:", Home_work,',' "затрачено часов:", Number_of_hours, ',' "среднее время выполнения", Time1, "часа.")
+print("Курс:", Course + ",", "всего задач:", f"{Home_work}," , "затрачено часов:", f"{Number_of_hours},", "среднее время выполнения", Time1, "часа.")
